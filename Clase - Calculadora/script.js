d3e813d1816a27f6dd4d657d55699c382ff898b2
Javier <javi13879@gmail.com>
2021-12-08 11:29:51 -0300

    Commit message

---
+++ Clase - Calculadora/script.js
@@ -0,0 +1,11 @@
+function mostrarMnesaje(parametro) {
+    parametro.prevenirDefault();
+    var uno = document.querySelector("#uno").value;
+    var dos = document.querySelector("#dos").value;
+    var resultado = parseFloat(uno)/parseFloat(dos);
+    mensaje = uno+"/"+ dos +" = "+ resultado;
+    document.querySelector("#mensaje").textContent = mensaje;
+}
+
+var formulario = document.querySelector("#formulario");
+formulario.addEventListener("input", mostrarMnesaje);
